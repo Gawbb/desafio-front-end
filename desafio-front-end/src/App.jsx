28b8900e60de44984c0f9e1503b752286e1aa8a9
Gabriela  Iaccino <gabriela.iaccino@hotmail.com>
2023-10-22 14:07:32 -0300

    Foi adicionada layout de formulário multi etapas

---
+++ desafio-front-end/src/App.jsx
@@ -0,0 +1,13 @@
+import MultiStepForm from './componentes/MultiStepForm';
+import { Box } from '@mui/material';
+
+function App() {
+  return (
+    <Box sx={{}}>
+      <h1>Formulário de Múltiplas Etapas</h1>
+      <MultiStepForm />
+    </Box>
+  );
+}
+
+export default App;
